8221841b0b285666ed566188e40ebc9a254a19ea
smrmkt <smrmkt@gmail.com>
2014-06-29 00:14:03 +0900

    add user_timeline streaming api method
    - add auto reply method for mention tweet
    - add streaming listerner class
    - modify conf key to screen name

--- src/lib/model/account.py
+++ src/lib/model/account.py
@@ -16,20 +16,20 @@ class Account:
         if conf_path is not None:
             self.__conf_path = conf_path
         self.__backup_dir = script_path + '/../../backup/' + screen_name
-        self.__mention_path = self.__backup_dir + 'last_mention.bak'
+        self.__mention_path = self.__backup_dir + '/last_mention.bak'
         if mention_path is not None:
             self.__mention_path = mention_path
 
         # initialize tweepy api
         conf = ConfigParser.SafeConfigParser()
         conf.read(os.path.dirname(__file__) + self.__conf_path)
-        auth = tweepy.OAuthHandler(
+        self.__auth = tweepy.OAuthHandler(
             conf.get(screen_name, 'consumer_key'),
             conf.get(screen_name, 'consumer_secret'))
-        auth.set_access_token(
+        self.__auth.set_access_token(
             conf.get(screen_name, 'access_token_key'),
             conf.get(screen_name, 'access_token_secret'))
-        self.__api = tweepy.API(auth)
+        self.__api = tweepy.API(self.__auth)
 
     def info(self):
         return self.__api.get_user(screen_name=self.__screen_name)
@@ -58,3 +58,6 @@ class Account:
             with open(self.__mention_path, 'w') as f:
                f.write(str(last_mention))
         return mentions
+
+    def userstream(self, listener):
+        return tweepy.Stream(self.__auth, listener).userstream()
