ae030938dd6bb3774111dbf0328951036c1ed35c
smrmkt <smrmkt@gmail.com>
2014-06-28 09:35:57 +0900

    modify conf_path setting

--- src/lib/model/account.py
+++ src/lib/model/account.py
@@ -8,19 +8,20 @@ script_path = os.path.dirname(__file__)
 script_path = script_path if len(script_path) else '.'
 
 class Account:
-    def __init__(self, screen_name, conf_path=None):
+    def __init__(self, screen_name, conf_path=None, mention_path=None):
         # set user screen name
         self.__screen_name = screen_name
         self.__conf_path = '/../../conf/api_keys.conf'
+        if conf_path is not None:
+            self.__conf_path = conf_path
         self.__mention_path = script_path + '/../../backup/' +\
                               screen_name + '/last_mention.bak'
+        if mention_path is not None:
+            self.__mention_path = mention_path
 
         # initialize API
         conf = ConfigParser.SafeConfigParser()
-        if conf_path != None:
-            conf.read(os.path.dirname(__file__) + conf_path)
-        else:
-            conf.read(os.path.dirname(__file__) + self.__conf_path)
+        conf.read(os.path.dirname(__file__) + self.__conf_path)
         self.__api = twitter.Api(
             consumer_key=conf.get(screen_name, 'consumer_key'),
             consumer_secret=conf.get(screen_name, 'consumer_secret'),
