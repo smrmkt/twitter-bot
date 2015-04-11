8429e3867e087251706d66361aca1109a882d05d
smrmkt <smrmkt@gmail.com>
2015-04-11 15:21:59 +0900

    change matchng method

--- src/account/noconoco_stream.py
+++ src/account/noconoco_stream.py
@@ -19,8 +19,6 @@ from noconoco_stock import NoconocoStock
 from noconoco_weather import NoconocoWeather
 from time import sleep
 
-recipe_pattern = re.compile('(献立|夕食|夕飯|昼食|昼飯|ご飯|ごはん|レシピ)')
-
 class NoconocoStream:
     def __init__(self, owner='', conf_path=None):
         self.__account = Account('noconoco_bot', conf_path)
@@ -79,7 +77,7 @@ class NoconocoStreamListener(tweepy.StreamListener):
                     for message in messages:
                         self.__account.post(message, status.id_str)
                         sleep(1)
-                elif recipe_pattern.match(target):
+                elif target.find('献立') > -1:
                     message = recipe_bot.get_reply_message(status)
                     self.__account.post(message, status.id_str)
                 else:
